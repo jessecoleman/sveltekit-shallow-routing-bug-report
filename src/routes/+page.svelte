eddc0f517159c13ee19663c13c23f3699d0efc84
Cole Chamberlin <cchamberlin@apptek.com>
2024-02-05 17:24:15 -0800

    better comment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,6 +19,11 @@
 
 {#if mounted}
   <Example />
+  <!--
+    When Example.svelte is mounted, it sets the $page.state,
+    then a bit later it calls `invalidateAll()`. This causes the
+    `layout.server.ts` to rerun, and the $page.state.id gets reset
+  -->
   loaded: {data.loaded}
   id: {$page.state.id};
 {/if}
